a3e6de74e3ea3aab00e54e8556f749df622c1dfd
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-28 11:22:28 +0100

    Simple counter created

---
+++ src/Counter.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class Counter extends React.Component {
+
+    state = {
+        value: 0
+    }
+
+    incHandler = () => this.setState({value: this.state.value + 1})
+    decHandler = () => this.setState({value: this.state.value - 1})
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.value}</h1>
+                <button
+                    onClick={this.incHandler}
+                >
+                +
+                </button>
+                <button
+                    onClick={this.decHandler}
+                >-
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Counter
