1530fe676da31511d09359e7dd6ce4ffe81d63f6
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-28 11:31:23 +0100

    Adding state to firebase

--- src/Counter.js
+++ src/Counter.js
@@ -6,8 +6,22 @@ class Counter extends React.Component {
         value: this.props.initialValue || 0
     }
 
-    incHandler = () => this.setState({value: this.state.value + 1})
-    decHandler = () => this.setState({value: this.state.value - 1})
+    incHandler = () => {
+        this.setState({value: this.state.value + 1})
+        let newValue = this.state.value + 1
+        fetch('https://krystian-kaminski.firebaseio.com/counter.json', {
+            method: 'PUT',
+            body: JSON.stringify(newValue)
+        })
+    }
+    decHandler = () =>  {
+        this.setState({value: this.state.value - 1})
+        let newValue = this.state.value - 1
+        fetch('https://krystian-kaminski.firebaseio.com/counter.json', {
+            method: 'PUT',
+            body: JSON.stringify(newValue)
+        })
+    }
 
     render() {
         return (
