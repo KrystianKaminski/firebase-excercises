8b2eafeba8508aa5c6961f2dd4d5b54a61c838ba
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-29 10:45:05 +0100

    Installed material-ui

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+ReactDOM.render(
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+, document.getElementById('root'));
